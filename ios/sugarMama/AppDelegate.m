e401669b4ec3066b9f29ce432808614f865904ed
michaelkoo3232 <56055915+michaelkoo3232@users.noreply.github.com>
2021-08-17 22:37:58 +0800

    ios latest

--- ios/sugarMama/AppDelegate.m
+++ ios/sugarMama/AppDelegate.m
@@ -115,7 +115,11 @@ didReceiveNotificationResponse:(UNNotificationResponse *)response
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:self.launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge moduleName:@"main" initialProperties:nil];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
+     if (@available(iOS 13.0, *)) {
+          rootView.backgroundColor = [UIColor systemBackgroundColor];
+      } else {
+          rootView.backgroundColor = [UIColor whiteColor];
+      }
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
